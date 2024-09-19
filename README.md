b3b430c72475fc11ce203b0933408f7becc3f6b9
jkurkure <117855264+jkurkure@users.noreply.github.com>
2024-09-19 20:45:33 +0000

    main source

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ghost
-Utility to keep running commands on a schedule
+Utility to keep running commands on a schedule. It works on both Windows and Linux, and is a lot easier to set-up than a cronjob. The default frequency is 1 per minute, but you can adjust this within the source code.
---
+++ ghost.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import os, time, sys, platformdirs, subprocess
+
+start = time.time()
+data_dir = platformdirs.user_data_dir(appname="ghost", appauthor="jkurkure")
+if not os.path.exists(data_dir):
+    os.makedirs(data_dir)
+file_path = os.path.join(data_dir, f"{start}.log")
+
+if (len(sys.argv) > 1):
+    print(f"{' '.join(sys.argv[1:])} has been scheduled to run every minute and logs can be found at {file_path}")
+
+    command = f"{' '.join(sys.argv[1:])} >> {file_path}"
+    script = f'''
+import os, time
+while True:
+    os.system(r"{command}")
+    time.sleep(60.0 - ((time.time() - {start}) % 60.0))
+    '''
+
+    helper = os.path.join(data_dir, f"{start}.pyw")
+    with open(helper, "w") as f:
+        f.write(script)
+
+    # spinlock until helper exists
+    while not os.path.exists(helper):
+        pass
+    subprocess.Popen(["python3", helper])
