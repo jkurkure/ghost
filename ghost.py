dad85bdcb339b55f5b04d49c23bf1ee8e371b7ef
jkurkure <117855264+jkurkure@users.noreply.github.com>
2024-09-19 22:42:43 +0100

    Add timestamp to logs

--- ghost.py
+++ ghost.py
@@ -2,7 +2,7 @@
 import os, time, sys, platformdirs, subprocess
 
 start = time.time()
-data_dir = platformdirs.user_data_dir(appname="ghost", appauthor="jkurkure")
+data_dir = platformdirs.user_data_dir(appname="ghost", appauthor="r93817dn")
 if not os.path.exists(data_dir):
     os.makedirs(data_dir)
 file_path = os.path.join(data_dir, f"{start}.log")
@@ -10,10 +10,12 @@ file_path = os.path.join(data_dir, f"{start}.log")
 if (len(sys.argv) > 1):
     print(f"{' '.join(sys.argv[1:])} has been scheduled to run every minute and logs can be found at {file_path}")
 
+    stamp = f"echo {time.ctime()} >> {file_path}"
     command = f"{' '.join(sys.argv[1:])} >> {file_path}"
     script = f'''
 import os, time
 while True:
+    os.system(r"{stamp}")
     os.system(r"{command}")
     time.sleep(60.0 - ((time.time() - {start}) % 60.0))
     '''
@@ -25,4 +27,4 @@ while True:
     # spinlock until helper exists
     while not os.path.exists(helper):
         pass
-    subprocess.Popen(["python3", helper])
+    subprocess.Popen(["pythonw", helper])
